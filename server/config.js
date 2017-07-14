aca2d4dea6009c62080c6141bd85e6c77326febd
Afreda323 <anthonyfreda323@gmail.com>
2017-07-14 15:19:57 -0400

    Setup for heroku mongo

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    mongoURI: 'mongodb://localhost:27017/auth',
+    mongoURI:  process.env.MONGODB_URI || 'mongodb://localhost:27017/auth',
     secret: 'dasitmane'
 }
