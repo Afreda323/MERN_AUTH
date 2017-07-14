cc8f9f02ebf8bce0f9e5a649ec399feb2066ceca
Afreda323 <anthonyfreda323@gmail.com>
2017-07-14 15:56:30 -0400

    ENV error

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,3 +3,4 @@ export const LOGIN_FAIL = "login_fail";
 export const SIGNUP = "signup";
 export const LOGOUT = "logout";
 export const GET_DATA = "get_data";
+export const API_URL = process.env.NODE_ENV === 'production' ? "https://intelligent-croissant-93086.herokuapp.com/api" : 'http://localhost:3000/api';
