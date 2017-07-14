be9a901002754af2e16e92ae81a11cc93c433195
Afreda323 <anthonyfreda323@gmail.com>
2017-07-14 16:01:45 -0400

    api_url setup

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,4 +3,5 @@ export const LOGIN_FAIL = "login_fail";
 export const SIGNUP = "signup";
 export const LOGOUT = "logout";
 export const GET_DATA = "get_data";
-export const API_URL = process.env.NODE_ENV === 'production' ? "https://intelligent-croissant-93086.herokuapp.com/api" : 'http://localhost:3000/api';
+// IF LOCAL USE YOUR SERVER URL ie localhost:3000
+export const API_URL = "https://intelligent-croissant-93086.herokuapp.com/api";
